ab984dbecc7de11910167f638873d66017df66de
mohadeseAhval <m.ahval303@gmail.com>
2019-09-17 12:18:54 +0430

    first excercise about anonymous functions

---
+++ oop-1.php
@@ -0,0 +1,37 @@
+<?php 
+
+
+ class CalculatorStrategy {
+   private $actionClasure;
+
+   function __construct($action) {
+      $this->actionClasure = $action;
+   }
+
+   public function doAction($f, $s) {
+    return call_user_func($this->actionClasure, $f, $s);
+   }
+ }
+
+ class Calculator {
+   private $firstNumber;
+   private $secondNumber;
+
+   public function setNumbers($f, $s) {
+    $this->firstNumber = $f;
+    $this->secondNumber = $s;
+   }
+
+   public function calculate(CalculatorStrategy $action) {
+    return $action->doAction($this->firstNumber, $this->secondNumber);
+   }
+ }
+ 
+
+$multiplication=new CalculatorStrategy(function ($firstNumber,$secondNumber){
+return $firstNumber * $secondNumber;
+});
+
+$object=new Calculator();
+$object->setNumbers(2,5);
+echo $object->calculate($multiplication);
